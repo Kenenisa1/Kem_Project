ce0dfaafbdc5e7a57e45c96b7ac70d412e96efc0
KENENISA MIESO <kenenisamiesob@Gmail.com>
2025-04-23 12:00:10 +0000

    Add onClick function and its handler for count

--- React_practice/src/App.jsx
+++ React_practice/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
+import MyButton from './MyButton'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  function handleCount()
+  {
+    setCount(count+1);
+  }
 
   return (
     <>
-      
+      <h1>Handle count Function</h1>
+      <MyButton count={count} onClick={handleCount}/>
+      <MyButton count={count} onClick={handleCount}/>
+
     </>
   )
 }
