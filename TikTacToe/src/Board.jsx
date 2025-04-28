e86ea0c9bcaa809c1ae3466efc8a27e4b3aac4e7
KENENISA MIESO <kenenisamiesob@Gmail.com>
2025-04-28 09:04:52 +0000

    Add calculateWinner function to determine game winner

--- TikTacToe/src/Board.jsx
+++ TikTacToe/src/Board.jsx
@@ -8,15 +8,18 @@ export default function Board()
 
     function handleClick(i)
     {
-        if(squares[i])
+        if(calculateWinner(squares) || squares[i])
         {
-            return
+            return;
         }
+
         const nextSquares= squares.slice();
+
         if(xIsNext)
         {
             nextSquares[i]='K';
         }
+
         else
         {
             nextSquares[i]='M';
@@ -24,6 +27,17 @@ export default function Board()
 
         setSquares(nextSquares);
         setXIsNext(!xIsNext);
+    }
+    
+    const winner=calculateWinner(squares);
+    let status ;
+    if(winner)
+    {
+        status='Winner : ' + winner;
+    }
+    else
+    {
+        status="Next Player " + ( xIsNext? 'K' : 'M')
     }
     return(
         <>
@@ -45,6 +59,31 @@ export default function Board()
             <Square value={squares[7]  } onSquareClick={()=> handleClick(7)}/>
             <Square value={squares[8]  } onSquareClick={()=> handleClick(8)}/>   
         </div>
+        <div> {status}</div>
         </>
     )
+}
+function calculateWinner(squares)
+{
+    const lines=[
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
+    for(let i=0;i<lines.length;i++)
+    {
+        const [a,b,c]= lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a]===squares[c])
+        {
+            return squares[a];
+        }
+    }
+    return null;
+
 }
