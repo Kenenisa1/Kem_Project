8fd77afc53765d9917b48c556479c7af33d86729
KENENISA MIESO <kenenisamiesob@Gmail.com>
2025-05-01 06:46:08 +0000

    add calculateWinner function function to Board component

--- TicTacToe/src/Board.jsx
+++ TicTacToe/src/Board.jsx
@@ -1,6 +1,7 @@
 import Square from "./Square";
+import React from "react";
 
-function Board({ xIsNext, squares, onPlay }) {
+export default function Board({ xIsNext, squares, onPlay }) {
     function handleClick(i) {
       if (calculateWinner(squares) || squares[i]) {
         return;
@@ -42,4 +43,24 @@ function Board({ xIsNext, squares, onPlay }) {
         </div>
       </>
     );
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
